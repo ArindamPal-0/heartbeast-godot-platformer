cbd2c341fed4c66a3a39ad9c16ae1d368f0d7114
Arindam Pal <arindampal2000@gmail.com>
2022-09-10 00:31:19 +0530

    fixing y motion gravity and adding some constants

--- Player.gd
+++ Player.gd
@@ -1,6 +1,9 @@
 extends KinematicBody2D
 
 const UP: Vector2 = Vector2(0, -1)
+const GRAVITY = 20
+const SPEED = 200
+const JUMP_HEIGHT = -550
 var motion: Vector2 = Vector2()
 
 # NOTE: move_and_slide and is_on_floor is provided by KinematicBody2D
@@ -9,17 +12,17 @@ var motion: Vector2 = Vector2()
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta):
 
-	motion.y += 10
+	motion.y += GRAVITY
 	
 	if Input.is_action_pressed("ui_right"):
-		motion.x = 100
+		motion.x = SPEED
 	elif Input.is_action_pressed("ui_left"):
-		motion.x = -100
+		motion.x = -SPEED
 	else:
 		motion.x = 0
 
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
-			motion.y = -400
+			motion.y = JUMP_HEIGHT
 
-	var _final = move_and_slide(motion, UP)
+	motion = move_and_slide(motion, UP)
