d205d9be386048413b0b21fc33357619906d5e05
Arindam Pal <arindampal2000@gmail.com>
2022-09-10 00:25:54 +0530

    making player jump
    - making a object floor instead of default wall
    - in move_and_slide passing second argument as Vector(0, -1) makes that object a floor
    - simple jump mechanic after checking is_on_floor

--- Player.gd
+++ Player.gd
@@ -1,6 +1,9 @@
 extends KinematicBody2D
 
+const UP: Vector2 = Vector2(0, -1)
 var motion: Vector2 = Vector2()
+
+# NOTE: move_and_slide and is_on_floor is provided by KinematicBody2D
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -15,4 +18,8 @@ func _physics_process(_delta):
 	else:
 		motion.x = 0
 
-	var _final = move_and_slide(motion)
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_up"):
+			motion.y = -400
+
+	var _final = move_and_slide(motion, UP)
