d50e2f4fc9584b3b959e2c9a4c1c4e7df409c072
Arindam Pal <arindampal2000@gmail.com>
2022-09-10 23:58:08 +0530

    character smooth movment
    - adding acceleration and a max_speed
    - adding a sliding effect before completely stopping using lerp
    - more sliding when in air
    - adding falling animation
    - implementing edge slipping to avoid player hanging at the edge
    - edge slipping by changing collider to capsule shape

--- Player.gd
+++ Player.gd
@@ -2,7 +2,8 @@ extends KinematicBody2D
 
 const UP: Vector2 = Vector2(0, -1)
 const GRAVITY = 20
-const SPEED = 200
+const ACCELERATION = 50
+const MAX_SPEED = 200
 const JUMP_HEIGHT = -550
 var motion: Vector2 = Vector2()
 
@@ -13,23 +14,32 @@ var motion: Vector2 = Vector2()
 func _physics_process(_delta):
 
 	motion.y += GRAVITY
+	var friction: bool = false
 	
 	if Input.is_action_pressed("ui_right"):
-		motion.x = SPEED
+		motion.x = min(motion.x + ACCELERATION, MAX_SPEED)
 		$Sprite.flip_h = false
 		$Sprite.play("Run")
 	elif Input.is_action_pressed("ui_left"):
-		motion.x = -SPEED
+		motion.x = max(motion.x - ACCELERATION, -MAX_SPEED)
 		$Sprite.flip_h = true
 		$Sprite.play("Run")
 	else:
 		$Sprite.play("Idle")
-		motion.x = 0
-
+		friction = true
+		
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
 			motion.y = JUMP_HEIGHT
+
+		if friction:
+			motion.x = lerp(motion.x, 0, 0.2)
 	else:
-		$Sprite.play("Jump")
+		if motion.y < 0:
+			$Sprite.play("Jump")
+		else:
+			$Sprite.play("Fall")
+		if friction:
+			motion.x = lerp(motion.x, 0, 0.02)
 
 	motion = move_and_slide(motion, UP)
